552cf14d1481f3e84977919c9d5d77e30c323d16
yuanqing <hello@yuanqing.sg>
2014-08-29 00:26:19 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var aps = Array.prototype.slice;
+
+var segue = function(cb) {
+
+  var running = false;
+  var queue = [];
+  var nextArgs = [];
+
+  var next = function() {
+
+    var args = aps.call(arguments);
+    var err = args.shift(); // `err` is the first argument of the `this` callback
+    var arr;
+
+    if (err) { // exit on `err`
+      return cb(err);
+    }
+
+    if (queue.length) { // call the next function in the `queue`
+      arr = queue.shift();
+      if (arr[1].length) {
+        args = arr[1];
+      }
+      nextArgs = [];
+      arr[0].apply(next, args);
+    } else {
+      nextArgs = args; // save the arguments passed to the `this` callback
+      running = false;
+    }
+
+  };
+
+  var enqueue = function() {
+
+    var args = aps.call(arguments);
+    var fn = args.shift();
+
+    if (!queue.length && !running) {
+      running = true;
+      fn.apply(next, args.length ? args : nextArgs);
+    } else {
+      queue.push([fn, args]);
+    }
+    return enqueue;
+
+  };
+
+  return enqueue;
+
+};
+
+module.exports = exports = segue;
---
+++ spec/segue.spec.js
@@ -0,0 +1,112 @@
+/* globals describe, beforeEach, it, expect, jasmine */
+'use strict';
+
+var segue = require('..');
+
+describe('segue(cb)(fn [, arg, ...])', function() {
+
+  describe('calls each `fn` sequentially', function() {
+
+    describe('with `next` using arguments from the `this` callback of `prev`', function() {
+
+      var cb, prev, next;
+
+      beforeEach(function() {
+
+        cb = jasmine.createSpy();
+        prev = function(foo) {
+          var that = this;
+          expect(foo).toBe(1);
+          setTimeout(function() {
+            that(null, 'from prev');
+          }, 10);
+        };
+        next = function(foo) {
+          expect(foo).toBe('from prev');
+          expect(cb).not.toHaveBeenCalled();
+          this(null);
+        };
+
+      });
+
+      it('when `next` had been enqueued while `prev` was still running', function() {
+
+        segue(cb)(prev, 1)(next);
+
+      });
+
+      it('when `next` had been enqueued after `prev` had already returned', function() {
+
+        var s = segue(cb)(prev, 1);
+        setTimeout(function() {
+          s(next);
+        }, 100); // `prev` returned after 10ms
+
+      });
+
+    });
+
+    it('with `next` not using arguments from the `this` callback of `prev`', function(done) {
+
+      var cb = jasmine.createSpy();
+      var prev = function(foo) {
+        var that = this;
+        expect(foo).toBe(1);
+        setTimeout(function() {
+          that(null, 'from prev');
+        }, 10);
+      };
+      var next = function(foo) {
+        var that = this;
+        expect(foo).not.toBe('from prev');
+        expect(foo).toBe(2);
+        setTimeout(function() {
+          that(null);
+          expect(cb).not.toHaveBeenCalled();
+          done();
+        }, 10);
+      };
+
+      segue(cb)(prev, 1)(next, 2);
+
+    });
+
+  });
+
+  describe('passes error to `cb` when there was an error', function() {
+
+    it('in an asynchronous function', function(done) {
+
+      var cb = jasmine.createSpy();
+      var next = jasmine.createSpy();
+      var prev = function() {
+        var that = this;
+        setTimeout(function() {
+          that('error');
+          expect(cb).toHaveBeenCalledWith('error');
+          expect(next).not.toHaveBeenCalled();
+          done();
+        }, 10);
+      };
+
+      segue(cb)(prev)(next);
+
+    });
+
+    it('in a synchronous function', function() {
+
+      var cb = jasmine.createSpy();
+      var next = jasmine.createSpy();
+      var prev = function() {
+        this('error');
+        expect(cb).toHaveBeenCalledWith('error');
+        expect(next).not.toHaveBeenCalled();
+      };
+
+      segue(cb)(prev)(next);
+
+    });
+
+  });
+
+});
